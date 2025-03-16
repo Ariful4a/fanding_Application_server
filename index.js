13a3eb677e474b0b3cb7fc9216b2324da6bea625
Ariful4a <afkgamingno1@gmail.com>
2025-03-16 05:12:19 -0400

    server load single data get api operation compleate

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion , ObjectId} = require('mongodb');
 const express = require('express')
 const cors = require('cors');
 const dotenv = require('dotenv');
@@ -10,10 +11,12 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.5eg9x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -42,7 +45,36 @@ async function run() {
     });
 
 
- 
+    // data read 
+    app.get('/campaign', async (req, res) => {
+        const rusult = await FundingCollection.find().toArray();
+        res.send(rusult);
+    })
+
+    // data read by id
+    app.get('/campaign/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await FundingCollection.findOne(query);
+        res.send(result);
+    })
+
+    // data update by id
+    // app.put('/campaign/:id', async (req, res) => {
+    //     const id = req.params.id;
+    //     const query = { _id: new ObjectId(id) };
+    //     const update = req.body;
+    //     const result = await FundingCollection.updateOne(query, { $set: update });
+    //     res.send(result);
+    // })
+
+    // // data delete by id
+    // app.delete('/campaign/:id', async (req, res) => {
+    //     const id = req.params.id;
+    //     const query = { _id: new ObjectId(id) };
+    //     const result = await FundingCollection.deleteOne(query);
+    //     res.send(result);
+    // })
 
 
 
